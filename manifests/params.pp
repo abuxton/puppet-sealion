31dc4a967d7c5639051b76ac54cc65d7dbd45685
adam buxton <buxton.adam@gmail.com>
2013-12-11 12:39:18 +0000

    update dto add download params

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,5 +2,9 @@ class sealion::params{
   $key        = 'false'
   $build      = 'false'
   $proxy      = 'false'
-  $proxy_url  =''
+  $proxy_url  = ''
+  $sealion_url= 'https://s3.amazonaws.com/sealion.com/'
+  $32bit_tar  = 'sealion-agent-package_i686.tar.gz'
+  $64bit_tar  = 'sealion.com/sealion-agent-package_x86_64.tar.gz'
+  
 }
