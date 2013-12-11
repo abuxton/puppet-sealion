b642bb522513a45cf0f029a18a5157891cd250a7
adam buxton <buxton.adam@gmail.com>
2013-12-11 15:25:55 +0000

    update to install

--- manifests/stage.pp
+++ manifests/stage.pp
@@ -1,10 +1,14 @@
-class sealion:stage (
+class sealion::stage (
   $64bit  = hiera('sealion::64bit',$sealion::params::64bit)
-) inherits sealion:params  {
+) inherits sealion::params  {
   include staging
-  if str2bool($64bit)
-    staging::file { $sealion::params::64bit_tar:
-      source => "${sealion::params::sealion_url}/${sealion::params::64bit_tar}"
+  file { '/tmp/staging/'  :
+    ensure => directory,
+  }
+
+  if str2bool($64bit) {
+    staging::file { "${sealion::params::64bit_tar}":
+      source => "${sealion::params::sealion_url}/${sealion::params::64bit_tar}",
     } 
     staging::extract { $sealion::params::64bit_tar:
       target  => '/tmp/staging',
@@ -13,13 +17,32 @@ class sealion:stage (
     }
   }
   else  {
-    staging::file { $sealion::params::32bit_tar:
+    staging::file { "${sealion::params::32bit_tar}":
       source => "${sealion::params::sealion_url}/${sealion::params::32bit_tar}"
     } 
     staging::extract { $sealion::params::32bit_tar:
       target  => '/tmp/staging',
-      creates => '/tmp/staging/$sealion::params::32bit_tar',
+      creates => '/tmp/staging/sealion-agent-package',
       require => Staging::File[$sealion::params::32bit_tar],
     }
   }
+  package  { 'make':
+    enure => present,
+  }
+
+  exec  { 'sealion Configure Makefile':
+    cmd     =>  "/tmp/staging/sealion-agent-package/configure -o ${sealion::params::key}",
+    require =>  Package['make'],
+    creates =>  '/tmp/staging/sealion/agent-package/Makefile',
+    notify  =>  Exec['sealion Install Agent'],
+  }
+
+  exec  { 'sealion Install Agent':
+    cmd     =>  'sudo /tmp/staging/sealion-agent-package/make install',
+    creates => '/usr/local/sealion-agent',
+
+
+
+
+
 }
