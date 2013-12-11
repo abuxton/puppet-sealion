024ae4aab3ec4a936f0d8190350be578055bbf43
adam buxton <buxton.adam@gmail.com>
2013-12-11 17:45:36 +0000

    build from source

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class sealion::params{
   $build      = 'false'
   $proxy      = 'false'
   $proxy_url  = ''
-  $sealion_url= 'https://s3.amazonaws.com/sealion.com/'
+  $sealion_url= 'https://s3.amazonaws.com/sealion.com'
   $64bit      = 'false' #control 64/32bit install
   $32bit_tar  = 'sealion-agent-package_i686.tar.gz'
   $64bit_tar  = 'sealion.com/sealion-agent-package_x86_64.tar.gz'
--- manifests/stage.pp
+++ manifests/stage.pp
@@ -14,6 +14,7 @@ class sealion::stage (
       target  => '/tmp/staging',
       creates => '/tmp/staging/$sealion::params::64bit_tar',
       require => Staging::File[$sealion::params::64bit_tar],
+      notify  => Exec['sealion Configure Makefile'],
     }
   }
   else  {
@@ -24,23 +25,29 @@ class sealion::stage (
       target  => '/tmp/staging',
       creates => '/tmp/staging/sealion-agent-package',
       require => Staging::File[$sealion::params::32bit_tar],
+      notify  => Exec['sealion Configure Makefile'],
     }
   }
   package  { 'make':
-    enure => present,
+    ensure => present,
   }
 
   exec  { 'sealion Configure Makefile':
-    cmd     =>  "/tmp/staging/sealion-agent-package/configure -o ${sealion::params::key}",
+    command =>  "./configure -o ${sealion::params::key}",
+    path    =>  '/usr/bin:/bin:/tmp/staging/sealion-agent-package',
+    cwd     =>  '/tmp/staging/sealion-agent-package',
     require =>  Package['make'],
-    creates =>  '/tmp/staging/sealion/agent-package/Makefile',
+    creates =>  '/tmp/staging/sealion-agent-package/Makefile',
     notify  =>  Exec['sealion Install Agent'],
   }
 
   exec  { 'sealion Install Agent':
-    cmd     =>  'sudo /tmp/staging/sealion-agent-package/make install',
+    command =>  'sudo make install', 
+    path    =>  '/usr/bin:bin',
+    cwd     =>  '/tmp/staging/sealion-agent-package',
+    require =>  Package['make'],
     creates => '/usr/local/sealion-agent',
-
+  }
 
 
 
