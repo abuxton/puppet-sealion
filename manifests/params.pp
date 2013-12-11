cc9c46b6dd289ab38be66b9f7d3e65f164e9b013
adam buxton <buxton.adam@gmail.com>
2013-12-11 14:20:34 +0000

    support for staging used to deploy tar.gz

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,7 @@ class sealion::params{
   $proxy      = 'false'
   $proxy_url  = ''
   $sealion_url= 'https://s3.amazonaws.com/sealion.com/'
+  $64bit      = 'false' #control 64/32bit install
   $32bit_tar  = 'sealion-agent-package_i686.tar.gz'
   $64bit_tar  = 'sealion.com/sealion-agent-package_x86_64.tar.gz'
   
--- manifests/stage.pp
+++ manifests/stage.pp
@@ -1,5 +1,25 @@
 class sealion:stage (
+  $64bit  = hiera('sealion::64bit',$sealion::params::64bit)
 ) inherits sealion:params  {
   include staging
-  
+  if str2bool($64bit)
+    staging::file { $sealion::params::64bit_tar:
+      source => "${sealion::params::sealion_url}/${sealion::params::64bit_tar}"
+    } 
+    staging::extract { $sealion::params::64bit_tar:
+      target  => '/tmp/staging',
+      creates => '/tmp/staging/$sealion::params::64bit_tar',
+      require => Staging::File[$sealion::params::64bit_tar],
+    }
+  }
+  else  {
+    staging::file { $sealion::params::32bit_tar:
+      source => "${sealion::params::sealion_url}/${sealion::params::32bit_tar}"
+    } 
+    staging::extract { $sealion::params::32bit_tar:
+      target  => '/tmp/staging',
+      creates => '/tmp/staging/$sealion::params::32bit_tar',
+      require => Staging::File[$sealion::params::32bit_tar],
+    }
+  }
 }
