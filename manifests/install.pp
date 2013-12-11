47e7e25c6fcecf1a63f125dbd84d26e6e9c1e1e5
adam buxton <buxton.adam@gmail.com>
2013-12-11 13:56:29 +0000

    update to make proxy single include

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,18 @@
 class sealion::install (
 ) inherits sealion::params{
+
   case $operatingsystem {
     'RedHat', 'CentOS'  :{
       include redhat
-      include installproxy
+
     }
     /^(Debian|Ubuntu)$/ :{
       include debian
-      include installproxy
     }
     default            :{
       fail("unsupported operating system")
     }
   }
+  include installproxy
+  
 }
